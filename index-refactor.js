cc258d6bd44a663aea099856f502ef3de9699f12
Amrio <amriogm@gmail.com>
2014-08-26 23:08:15 +0800

    未完成

---
+++ index-refactor.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var program = require('commander')
+var grunt = require('grunt')
+var taskMain = require('./lib/task-main')
+
+var pkg = (function() {
+    var cwd = process.cwd()
+    process.chdir(__dirname)
+    var json = grunt.file.readJSON('package.json')
+    process.chdir(cwd)
+    return json
+})()
+
+program.version(pkg.version)
+    .option('--build [path]', '构建路径')
+    .option('--dist [path]', '部署路径', 'sea-modules')
+    .option('--include [option]', '构建包含范围', 'relative')
+    .option('--paths [path]', 'paths 路径')
+    .option('--force', '强制执行')
+    .parse(process.argv)
+
+// taskMain(program.input, program.output, program.include, program)
+taskMain(grunt, 'test/mod', 'test/dist', 'all')
--- test/dist/mod/b/b.js
+++
@@ -1,1 +0,0 @@
-define("mod/b/b",[],function(){return"b"});
