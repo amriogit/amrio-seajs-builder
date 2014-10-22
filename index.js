5fb792d4f3ba92ec2564da23261f8845549fec65
Amrio <amriogm@gmail.com>
2014-10-22 23:56:21 +0800

    初步完成

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-try{
 var fs = require('fs'),
     path = require('path'),
     util = require('util')
@@ -6,159 +5,194 @@ var fs = require('fs'),
 var glob = require('glob')
 var cmd = require('cmd-util')
 var grunt = require('grunt')
+var UglifyJS = cmd.UglifyJS
 
-process.chdir('test/assets/')
+function main(options) {
+    var cache = {},
+        paths = ['sea-modules']
 
-var cache = {},
-    paths = ['.tmp', 'sea-modules']
-
-function build(options) {
-    cache = {}
-    var result = []
-    var files = glob.sync(path.join(options.src, '**/*.js'))
-
-    files.forEach(function(filepath) {
-        var data = recursiveConcat(filepath)
-        grunt.file.write(path.join(options.dest, filepath), data)
-        console.log('Build %s success.\n', ('"' + filepath + '"').cyan)
-    })
-}
-
-function recursiveConcat(filepath) {
-    if (cache[id]) {
-        return cache[id]
-    }
-
-    var result = ''
-    var id = filepath.replace('.js', '')
-    var data = fs.readFileSync(filepath).toString()
-    var ast = cmd.ast.getAst(data)
-    var deps = getDepsByAst(ast, path.dirname(id))
-
-    ast = cmd.ast.modify(ast, {
-        id: id,
-        dependencies: deps.remain
-    })
-
-    result = ast.print_to_string({
-        beautify: true,
-        comments: true
-    })
-
-    if (deps.files.length) {
-        result += '\n\n' + deps.files.join('\n\n')
-    }
-
-    cache[id] = result
-
-    return result
-}
-
-function getDepsByAst(ast, base) {
-    var records = {}
-    var result = {
-        remain: [],
-        files: []
-    }
-
-    cmd.ast.parseFirst(ast).dependencies.forEach(function(dep) {
-        if (result.remain.indexOf(dep) === -1) {
-            var file = getFile(dep, base)
-            if (file === undefined) {
-                result.remain.push(dep)
-            } else {
-                if (!records[dep]) {
-                    updateRecords(file)
-                    result.files.push(file)
-                }
-            }
+    function start(options) {
+        var filespath = []
+        if (fs.statSync(options.src).isFile()) {
+            filespath = filespath.concat(options.src)
         } else {
-            result.remain.push(dep)
+            filespath = glob.sync(path.join(options.src, '**/*.js'))
         }
-    })
 
-    function updateRecords(file) {
-        cmd.ast.parse(file).forEach(function(m) {
-            records[m.id] = true
+        if(options.paths) {
+            paths = paths.concat(options.paths)
+        }
+
+        filespath.forEach(function(filepath) {
+            var data = concat(filepath)
+            data = cleanCode(data)
+            grunt.file.write(path.join(options.dest, filepath), data)
+            console.log('Build %s success.', ('"' + filepath + '"').cyan)
         })
     }
 
-    return result
-}
-
-function getDefineIdsInData(ast) {
-    return cmd.ast.parse(ast).filter(function(m) {
-        return m.id
-    }).map(function(m) {
-        return m.id
-    })
-}
-
-function getFile(id, base) {
-    var ext = path.extname(id)
-
-    if (!ext) {
-        id += '.js'
-    }
-
-    if (id.charAt(0) === '.') {
-        id = cmd.iduri.normalize(path.join(base, id))
-        if (ext === '.css') {
-            return css2js(id)
+    function concat(filepath) {
+        if (cache[id]) {
+            return cache[id]
         }
-        return recursiveConcat(id)
-    } else {
-        if (ext === '.css') {
-            return css2js(findIdInPaths(id))
+
+        if (!fs.existsSync(filepath)) {
+            return ''
         }
-        return fs.readFileSync(findIdInPaths(id)).toString()
+
+        var id = filepath.replace(/\.js$/, '')
+        var data = fs.readFileSync(filepath).toString()
+        var ast = cmd.ast.getAst(data)
+        var deps = getModuleAllDeps(ast, path.dirname(id))
+
+        ast = cmd.ast.modify(ast, {
+            id: id,
+            dependencies: deps.remain
+        })
+
+        var result = ast.print_to_string()
+
+        if (deps.files.length) {
+            result += '\n\n' + deps.files.join('\n\n')
+        }
+
+        cache[id] = result
+
+        return result
     }
-}
 
-function findIdInPaths(id) {
-    var result = null
-    var has = paths.some(function(p) {
-        var filepath = path.join(p, id)
-        var exists = fs.existsSync(filepath)
-        exists && (result = cmd.iduri.normalize(filepath))
-        return exists
-    })
+    function getModuleAllDeps(ast, base) {
+        ast = cmd.ast.getAst(ast)
 
-    if (!has) {
-        console.log('Can\'t find top-level ID %s', ('"' + id + '"').cyan)
+        var records = {}
+        var result = {
+            remain: [],
+            files: []
+        }
+
+        var meta = cmd.ast.parseFirst(ast)
+
+        if (!meta) {
+            return result
+        }
+
+        meta.dependencies.forEach(function(dep) {
+            if (result.remain.indexOf(dep) === -1) {
+                var file = getFile(dep, base)
+                if (file === undefined) {
+                    result.remain.push(dep)
+                } else {
+                    if (!records[dep]) {
+                        result.remain = grunt.util._.union(result.remain, getModuleAllDeps(file, dep).remain)
+                        updateRecords(file)
+                        result.files.push(file)
+                    }
+                }
+            } else {
+                result.remain.push(dep)
+            }
+        })
+
+        function updateRecords(file) {
+            cmd.ast.parse(file).forEach(function(m) {
+                records[m.id] = true
+            })
+        }
+
+        return result
     }
-    return result
-}
 
-function css2js(id) {
-    var tpl = [
-        'define("%s", [], function() {',
-        '   seajs.importStyle(%s)',
-        '});'
-    ].join('\n')
+    function getFile(id, base) {
+        var ext = path.extname(id),
+            filepath
 
-    var code = fs.readFileSync(id).toString()
-    code = util.format(tpl, id, JSON.stringify(code).replace(/(\s|\\n|\\r)+/g, ' '))
-    return code
+        if (!ext || ext !== '.css') {
+            id += '.js'
+        }
+
+        if (id.charAt(0) === '.') {
+            filepath = cmd.iduri.normalize(path.join(base, id))
+            if (ext === '.css') {
+                return css2js(filepath)
+            }
+            return concat(filepath)
+        } else {
+            filepath = getAvailableFilePath(id)
+            if (!filepath) {
+                return
+            }
+            if (ext === '.css') {
+                return css2js(filepath)
+            }
+            return fs.readFileSync(filepath).toString()
+        }
+    }
+
+    function getAvailableFilePath(id) {
+        var result = null
+        if (path.dirname(id) === '.') {
+            return null
+        }
+        var has = paths.some(function(modulesPath) {
+            var filepath = path.join(modulesPath, id)
+            var exists = fs.existsSync(filepath)
+            exists && (result = cmd.iduri.normalize(filepath))
+            return exists
+        })
+
+        if (!has) {
+            console.log('Can\'t find top-level ID %s', ('"' + id + '"').cyan)
+        }
+
+        return result
+    }
+
+    function css2js(id) {
+        if(!fs.existsSync(id)) {
+            return ''
+        }
+        var tpl = [
+            'define("%s", [], function() {',
+            '   seajs.importStyle(%s)',
+            '});'
+        ].join('\n')
+
+        var code = fs.readFileSync(id).toString()
+        code = util.format(tpl, id, JSON.stringify(code).replace(/(\s|\\n|\\r)+/g, ' '))
+        return code
+    }
+
+    function cleanCode(code) {
+        var ast = UglifyJS.parse(code)
+        ast.figure_out_scope()
+        var compressor = UglifyJS.Compressor({
+            warnings: false
+        })
+        ast = ast.transform(compressor)
+        ast.figure_out_scope()
+        ast.compute_char_frequency()
+        ast.mangle_names()
+        return ast.print_to_string()
+    }
+
+    start(options)
 }
 
 function test() {
-    build({
+    process.chdir('test/assets/')
+
+    main({
         src: 'amrio',
-        dest: '.tmp'
+        dest: '.tmp',
+        paths: ['./']
     })
-    build({
+
+    main({
         src: 'biz',
-        dest: '.tmp'
+        dest: '.tmp',
+        paths: ['.tmp', './']
     })
-
-    cache
 }
 
-console.log(1)
 // test()
-module.exports = build
-
-}catch(e){
-    console.log(e)
-}
+module.exports = main
